07dc08d151821ca46727e57ab4e4ccedc36685ad
Thariq5113 <tthariq898@gmail.com>
2026-03-02 01:47:06 +0530

    Removed Terraform state files and added gitignore

---
+++ main.tf
@@ -0,0 +1,122 @@
+provider "aws" {
+  region = "ap-south-1"
+}
+
+resource "aws_api_gateway_rest_api" "devops_api" {
+  name        = "devops-api-level2"
+  description = "DevOps Assignment Level 2 - Terraform API Gateway"
+  endpoint_configuration {
+    types = ["REGIONAL"]
+  }
+}
+
+resource "aws_api_gateway_resource" "json" {
+  rest_api_id = aws_api_gateway_rest_api.devops_api.id
+  parent_id   = aws_api_gateway_rest_api.devops_api.root_resource_id
+  path_part   = "json"
+}
+resource "aws_api_gateway_resource" "json_proxy" {
+  rest_api_id = aws_api_gateway_rest_api.devops_api.id
+  parent_id   = aws_api_gateway_resource.json.id
+  path_part   = "{proxy+}"
+}
+resource "aws_api_gateway_method" "json_get" {
+  rest_api_id   = aws_api_gateway_rest_api.devops_api.id
+  resource_id   = aws_api_gateway_resource.json_proxy.id
+  http_method   = "GET"
+  authorization = "NONE"
+
+  request_parameters = {
+    "method.request.path.proxy" = true
+  }
+}
+resource "aws_api_gateway_integration" "json_integration" {
+  rest_api_id             = aws_api_gateway_rest_api.devops_api.id
+  resource_id             = aws_api_gateway_resource.json_proxy.id
+  http_method             = aws_api_gateway_method.json_get.http_method
+  integration_http_method = "GET"
+  type                    = "HTTP_PROXY"
+  uri                     = "https://jsonplaceholder.typicode.com/{proxy}"
+
+  request_parameters = {
+    "integration.request.path.proxy" = "method.request.path.proxy"
+  }
+}
+resource "aws_api_gateway_resource" "weather" {
+  rest_api_id = aws_api_gateway_rest_api.devops_api.id
+  parent_id   = aws_api_gateway_rest_api.devops_api.root_resource_id
+  path_part   = "weather"
+}
+resource "aws_api_gateway_method" "weather_get" {
+  rest_api_id   = aws_api_gateway_rest_api.devops_api.id
+  resource_id   = aws_api_gateway_resource.weather.id
+  http_method   = "GET"
+  authorization = "NONE"
+
+  request_parameters = {
+    "method.request.querystring.latitude"  = true
+    "method.request.querystring.longitude" = true
+    "method.request.querystring.hourly"    = false
+  }
+}
+resource "aws_api_gateway_integration" "weather_integration" {
+  rest_api_id             = aws_api_gateway_rest_api.devops_api.id
+  resource_id             = aws_api_gateway_resource.weather.id
+  http_method             = aws_api_gateway_method.weather_get.http_method
+  integration_http_method = "GET"
+  type                    = "HTTP_PROXY"
+  uri                     = "https://api.open-meteo.com/v1/forecast"
+
+  request_parameters = {
+    "integration.request.querystring.latitude"  = "method.request.querystring.latitude"
+    "integration.request.querystring.longitude" = "method.request.querystring.longitude"
+    "integration.request.querystring.hourly"    = "method.request.querystring.hourly"
+  }
+}
+resource "aws_api_gateway_resource" "countries" {
+  rest_api_id = aws_api_gateway_rest_api.devops_api.id
+  parent_id   = aws_api_gateway_rest_api.devops_api.root_resource_id
+  path_part   = "countries"
+}
+resource "aws_api_gateway_resource" "country_name" {
+  rest_api_id = aws_api_gateway_rest_api.devops_api.id
+  parent_id   = aws_api_gateway_resource.countries.id
+  path_part   = "{name}"
+}
+resource "aws_api_gateway_method" "country_get" {
+  rest_api_id   = aws_api_gateway_rest_api.devops_api.id
+  resource_id   = aws_api_gateway_resource.country_name.id
+  http_method   = "GET"
+  authorization = "NONE"
+
+  request_parameters = {
+    "method.request.path.name" = true
+  }
+}
+resource "aws_api_gateway_integration" "country_integration" {
+  rest_api_id             = aws_api_gateway_rest_api.devops_api.id
+  resource_id             = aws_api_gateway_resource.country_name.id
+  http_method             = aws_api_gateway_method.country_get.http_method
+  integration_http_method = "GET"
+  type                    = "HTTP_PROXY"
+  uri                     = "https://restcountries.com/v3.1/name/{name}"
+
+  request_parameters = {
+    "integration.request.path.name" = "method.request.path.name"
+  }
+}
+resource "aws_api_gateway_deployment" "deployment" {
+
+  depends_on = [
+    aws_api_gateway_integration.json_integration,
+    aws_api_gateway_integration.weather_integration,
+    aws_api_gateway_integration.country_integration
+  ]
+
+  rest_api_id = aws_api_gateway_rest_api.devops_api.id
+}
+resource "aws_api_gateway_stage" "v1" {
+  deployment_id = aws_api_gateway_deployment.deployment.id
+  rest_api_id   = aws_api_gateway_rest_api.devops_api.id
+  stage_name    = "v1"
+}
